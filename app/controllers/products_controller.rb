896867f29e8dbc6bab84985f54fd72b460015cdd
TarCode <tarcode33@gmail.com>
2022-10-11 18:00:26 +0200

    Added prices to product and updated product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,6 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!
   def index
     products = Product.all
-    render json: products, include: %i[store category]
+    render json: products, include: %i[store category prices]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,8 @@ store2 = Store.create(name: 'Obama Fried Chicken')
 category1 = Category.create(name: 'Burgers')
 category2 = Category.create(name: 'Chicken')
 
-Product.create(name: "Bob's Whopper", store: store1, category: category1)
-Product.create(name: 'Streetwise 9', store: store2, category: category2)
+product1 = Product.create(name: "Bob's Whopper", store: store1, category: category1)
+product2 = Product.create(name: 'Streetwise 9', store: store2, category: category2)
+
+Price.create(value_in_cents: 2000, unit: 'R', product: product1)
+Price.create(value_in_cents: 3000, unit: 'R', product: product2)
