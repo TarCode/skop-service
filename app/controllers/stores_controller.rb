100e12004d6d4f817cf2532351afb84c2ac64d02
TarCode <tarcode33@gmail.com>
2022-10-12 07:07:04 +0200

    Added show and create store controller methods

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -7,4 +7,20 @@ class StoresController < ApplicationController
     stores = Store.where(user: current_user)
     render json: stores
   end
+
+  def show
+    @store = Store.find(params[:id])
+    render json: @store
+  end
+
+  def create
+    store = Store.create!(name: store_params[:name], user: current_user)
+    render json: { message: 'Store added', store: }
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name)
+  end
 end
