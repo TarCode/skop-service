fed1a7d5b80361e4de12e4e24b9bcb37687087ef
TarCode <tarcode33@gmail.com>
2022-10-14 07:28:59 +0200

    Added validations on the sale and saleitem model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Sale < ApplicationRecord
+  validates :total_in_cents, :unit, :store_id, presence: true
   belongs_to :store
   belongs_to :user
   has_many :saleitems
--- app/models/saleitem.rb
+++ app/models/saleitem.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Saleitem < ApplicationRecord
+  validates :price_in_cents, :qty, :product_id, presence: true
   belongs_to :product
   belongs_to :sale
 end
